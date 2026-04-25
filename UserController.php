5216657d29238b3eec84eaeec287245bca079138
kana686 <inui.k.w6@gmail.com>
2026-04-25 12:58:14 +0900

    pagenate(20)に修正

--- UserController.php
+++ UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::pagenate(20);
         return view('users.index', ['users' => $users]);
     }
 
